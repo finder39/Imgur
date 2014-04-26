bd31e164ad0997cc8480099b557c3800f9a74591
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-04-26 09:44:56 -0700

    Added some API signatures to VWWRESTEngine

--- Imgur/Communication/VWWRESTEngine.h
+++ Imgur/Communication/VWWRESTEngine.h
@@ -28,11 +28,14 @@ typedef enum {
 
 -(void)setMode:(VWWRESTEngineMode)mode;
 
+// https://api.imgur.com/oauth2
+// https://api.imgur.com/oauth2/token
 -(MKNetworkOperation*)getTokensWithForm:(VWWCodeForm*)form
                         completionBlock:(VWWTokenBlock)completionBlock
                              errorBlock:(VWWErrorStringBlock)errorBlock;
 
-
+// https://api.imgur.com/endpoints/account
+// https://api.imgur.com/3/account/{username}
 -(MKNetworkOperation*)getAccountWithCompletionBlock:(VWWDictionaryBlock)completionBlock
                                          errorBlock:(VWWErrorStringBlock)errorBlock;
 
@@ -40,42 +43,46 @@ typedef enum {
 
 
 // https://api.imgur.com/endpoints/account#images
+// https://api.imgur.com/3/account/{username}/images/{page}
 -(MKNetworkOperation*)getImagesWithForm:(VWWPaginationForm*)form
                         completionBlock:(VWWArrayBlock)completionBlock
                              errorBlock:(VWWErrorStringBlock)errorBlock;
 
-//
-//
-//// https://api.imgur.com/endpoints/account#albums
+// https://api.imgur.com/endpoints/account#albums
+// https://api.imgur.com/3/account/{username}/albums/{page}
 //-(MKNetworkOperation*)getAlbumsWithForm:(VWWPaginationForm*)form
 //                        completionBlock:(VWWArrayBlock)completionBlock
 //                             errorBlock:(VWWErrorStringBlock)errorBlock;
 //
 //
-//// https://api.imgur.com/endpoints/account#comments
+// https://api.imgur.com/endpoints/account#comments
+// https://api.imgur.com/3/account/{username}/comments
 //-(MKNetworkOperation*)getCommentsWithForm:(VWWPaginationForm*)form
 //                        completionBlock:(VWWArrayBlock)completionBlock
 //                             errorBlock:(VWWErrorStringBlock)errorBlock;
 //
 //
-//// https://api.imgur.com/endpoints/account#replies
+// https://api.imgur.com/endpoints/account#replies
+// https://api.imgur.com/3/account/{username}/notifications/replies
 //-(MKNetworkOperation*)getRepliesWithForm:(VWWPaginationForm*)form
 //                          completionBlock:(VWWArrayBlock)completionBlock
 //                               errorBlock:(VWWErrorStringBlock)errorBlock;
 //
 //
-//// https://api.imgur.com/endpoints/account#account-profile
+// https://api.imgur.com/endpoints/account#account-profile
 //-(MKNetworkOperation*)getGalleryProfileWithForm:(VWWPaginationForm*)form
 //                         completionBlock:(VWWArrayBlock)completionBlock
 //                              errorBlock:(VWWErrorStringBlock)errorBlock;
 //
-//// https://api.imgur.com/endpoints/account#account-settings
+// https://api.imgur.com/endpoints/account#account-settings
+// https://api.imgur.com/3/account/{username}/settings
 //-(MKNetworkOperation*)getAccountSettingsWithForm:(VWWPaginationForm*)form
 //                                 completionBlock:(VWWArrayBlock)completionBlock
 //                                      errorBlock:(VWWErrorStringBlock)errorBlock;
 //
 //
-//// https://api.imgur.com/endpoints/account#update-settings
+// https://api.imgur.com/endpoints/account#update-settings
+// https://api.imgur.com/3/account/{username}/settings
 //-(MKNetworkOperation*)postAccountSettingsWithForm:(VWWHTTPForm*)form
 //                                  completionBlock:(VWWArrayBlock)completionBlock
 //                                       errorBlock:(VWWErrorStringBlock)errorBlock;
